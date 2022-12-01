af5b0ec31c6e4b1cf939ec0eb73bf3bc4f716148
Skick <skick1234@gmail.com>
2022-12-01 19:05:43 +0700

    feat: add options to provide client credentials

--- README.md
+++ README.md
@@ -14,40 +14,20 @@
 
 SoundCloud extractor plugin for [DisTube.js.org](https://distube.js.org).
 
-# Feature
+## Feature
 
 - Using SoundCloud API
 - Support SoundCloud tracks, albums and playlists
 - Search on SoundCloud
 - Faster than `youtube-dl` extractor
 
-# Installation
+## Installation
 
 ```sh
 npm install @distube/soundcloud@latest
 ```
 
-# Documentation
-
-### new SoundCloudPlugin()
-
-Create a DisTube's `ExtractorPlugin`
-
-### SoundCloudPlugin.search(query, [type], [limit]) _(Both `static` and `class` method)_
-
-Searches for the given query on SoundCloud.
-
-- Parameters
-
-  - `query` [string] Search query.
-  - `type` [string]: Type of results (`track` or `playlist`). Default is `track`.
-  - `limit` [integer]: Limit the results. Default is `10`.
-
-- Returns a `Promise<Song[]|Playlist[]>`
-  - Returns a `Promise<Song[]>` if `type` parameter is `track`
-  - Returns a `Promise<Playlist[]>` if `type` parameter is `playlist`
-
-# Usage
+## Usage
 
 ### Plugin
 
@@ -73,3 +53,33 @@ const scPlugin = new SoundCloudPlugin();
 scPlugin.search("A SoundCloud Playlist", "playlist", 3); // class method
 // Returns an array of 3 DisTube's Playlist
 ```
+
+## Documentation
+
+### new SoundCloudPlugin([SoundCloudPluginOptions])
+
+- `SoundCloudPluginOptions.clientId` [string] _(optional)_: Your account's client id.
+- `SoundCloudPluginOptions.oauthToken` [string] _(optional)_: Your account's oauth token. Used to fetch more data with SoundCloud Go+ account.
+
+#### Example
+
+```js
+new SpotifyPlugin({
+  clientId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  oauthToken: "0-000000-000000000-xxxxxxxxxxxxxx",
+});
+```
+
+### SoundCloudPlugin.search(query, [type], [limit]) _(Both `static` and `class` method)_
+
+Searches for the given query on SoundCloud.
+
+- Parameters
+
+  - `query` [string] Search query.
+  - `type` [string]: Type of results (`track` or `playlist`). Default is `track`.
+  - `limit` [integer]: Limit the results. Default is `10`.
+
+- Returns a `Promise<Song[]|Playlist[]>`
+  - Returns a `Promise<Song[]>` if `type` parameter is `track`
+  - Returns a `Promise<Playlist[]>` if `type` parameter is `playlist`
--- index.ts
+++ index.ts
@@ -1,74 +1,76 @@
 import SoundCloud from "soundcloud.ts";
-import { DisTubeError, ExtractorPlugin, Playlist, Song } from "distube";
+import { DisTubeError, ExtractorPlugin, Playlist, Song, checkInvalidKey } from "distube";
 import type { GuildMember } from "discord.js";
 import type { OtherSongInfo, PlaylistInfo } from "distube";
 import type { SoundcloudPlaylistV2, SoundcloudTrackV2 } from "soundcloud.ts";
-const sc = new SoundCloud();
+const SC = new SoundCloud();
 
 type Falsy = undefined | null | false | 0 | "";
 const isTruthy = <T>(x: T | Falsy): x is T => Boolean(x);
-type ElementOf<A> = A extends readonly (infer T)[] ? T : never;
-const SEARCH_SUPPORT = ["track", "playlist"] as const;
+export enum SearchType {
+  Track = "track",
+  Playlist = "playlist",
+}
 
-const chunker = (arr: any[], size: number) => {
-  const chunks = [];
-  let i = 0;
-  while (i < arr.length) chunks.push(arr.slice(i, (i += size)));
-  return chunks;
-};
-
-const resolveTracks = async (tracks: SoundcloudTrackV2[]): Promise<SoundcloudTrackV2[]> => {
-  const unsolved = tracks.splice(tracks.findIndex(t => !t.title));
-  const chunks = chunker(unsolved, 50);
-  const promises = chunks.map(ts => sc.api.getV2("/tracks", { ids: ts.map(t => t.id).join(",") }));
-  const solvedTracks = await Promise.all(promises);
-  return tracks.concat(solvedTracks.flat());
-};
+export interface SoundCloudPluginOptions {
+  clientId?: string;
+  oauthToken?: string;
+}
 
 export class SoundCloudPlugin extends ExtractorPlugin {
-  /**
-   * Search for tracks/playlists on SoundCloud
-   * @param {string} query String query
-   * @param {'track'|'playlist'} [type='track'] type
-   * @param {number} [limit=10] limit
-   * @returns {Array<Song>|Array<Playlist>}
-   */
-  static async search(query: string, type: ElementOf<typeof SEARCH_SUPPORT> = "track", limit = 10) {
-    if (typeof query !== "string") throw new DisTubeError("INVALID_TYPE", "string", query, "query");
-    if (!SEARCH_SUPPORT.includes(type)) throw new DisTubeError("INVALID_TYPE", SEARCH_SUPPORT, type, "type");
+  #sc: SoundCloud;
+  constructor(options: SoundCloudPluginOptions = {}) {
+    super();
+    if (typeof options !== "object" || Array.isArray(options)) {
+      throw new DisTubeError("INVALID_TYPE", ["object", "undefined"], options, "SoundCloudPluginOptions");
+    }
+    checkInvalidKey(options, ["clientId", "oauthToken"], "SoundCloudPluginOptions");
+    if (options.clientId && typeof options.clientId !== "string") {
+      throw new DisTubeError("INVALID_TYPE", "string", options.clientId, "clientId");
+    }
+    if (options.oauthToken && typeof options.oauthToken !== "string") {
+      throw new DisTubeError("INVALID_TYPE", "string", options.oauthToken, "oauthToken");
+    }
+    this.#sc = new SoundCloud(options?.clientId, options?.oauthToken);
+  }
+  static search(query: string, type?: SearchType.Track, limit?: number): Promise<Song<undefined>[]>;
+  static search(query: string, type: SearchType.Playlist, limit?: number): Promise<Playlist<undefined>[]>;
+  static search(query: string, type?: SearchType, limit?: number): Promise<Song<undefined>[] | Playlist<undefined>[]>;
+  static async search(query: string, type: SearchType = SearchType.Track, limit = 10) {
+    if (typeof query !== "string") {
+      throw new DisTubeError("INVALID_TYPE", "string", query, "query");
+    }
+    if (!Object.values(SearchType).includes(type)) {
+      throw new DisTubeError("INVALID_TYPE", Object.values(SearchType), type, "type");
+    }
     if (typeof limit !== "number" || limit < 1 || !Number.isInteger(limit)) {
       throw new DisTubeError("INVALID_TYPE", "natural number", limit, "limit");
     }
-    if (type === "track") {
-      const data = await sc.tracks.searchV2({ q: query, limit });
-      if (!data?.collection?.length) {
-        throw new DisTubeError("SOUNDCLOUD_PLUGIN_NO_RESULT", `Cannot find any "${query}" ${type} on SoundCloud!`);
+
+    switch (type) {
+      case SearchType.Track: {
+        const data = await SC.tracks.searchV2({ q: query, limit });
+        if (!data?.collection?.length) {
+          throw new DisTubeError("SOUNDCLOUD_PLUGIN_NO_RESULT", `Cannot find any "${query}" ${type} on SoundCloud!`);
+        }
+        return data.collection.map(t => new Song(new SoundCloudTrack(t)));
       }
-      return data.collection.map(t => new Song(new SoundCloudTrack(t)));
+      case SearchType.Playlist: {
+        const data = await SC.playlists.searchV2({ q: query, limit });
+        const playlists = data.collection;
+        return (
+          await Promise.all(playlists.map(async p => new Playlist(new SoundCloudPlaylist(await SC.playlists.fetch(p)))))
+        ).filter(isTruthy);
+      }
+      default:
+        throw new DisTubeError("SOUNDCLOUD_PLUGIN_UNSUPPORTED_TYPE", `${type} search is not supported!`);
     }
-    const data = await sc.playlists.searchV2({ q: query, limit });
-    const playlists = data.collection;
-    return (
-      await Promise.all(
-        playlists.map(async p => {
-          const playlist = new SoundCloudPlaylist(p);
-          if (!playlist.tracks?.length) return;
-          playlist.songs = (await resolveTracks(playlist.tracks)).map(s => new Song(new SoundCloudTrack(s)));
-          // eslint-disable-next-line consistent-return
-          return new Playlist(playlist);
-        }),
-      )
-    ).filter(isTruthy);
   }
 
-  /**
-   * Search for tracks/playlists on SoundCloud
-   * @param {string} query String query
-   * @param {'track'|'playlist'} [type='track'] type
-   * @param {number} [limit=10] limit
-   * @returns {Array<Song>|Array<Playlist>}
-   */
-  search(query: string, type: ElementOf<typeof SEARCH_SUPPORT> = "track", limit = 10) {
+  search(query: string, type?: SearchType.Track, limit?: number): Promise<Song<undefined>[]>;
+  search(query: string, type: SearchType.Playlist, limit?: number): Promise<Playlist<undefined>[]>;
+  search(query: string, type?: SearchType, limit?: number): Promise<Song<undefined>[] | Playlist<undefined>[]>;
+  search(query: string, type: SearchType = SearchType.Track, limit = 10) {
     return SoundCloudPlugin.search(query, type, limit);
   }
 
@@ -80,27 +82,23 @@ export class SoundCloudPlugin extends ExtractorPlugin {
   async resolve(url: string, options: { member?: GuildMember; metadata?: any }) {
     const opt = { ...options, source: "soundcloud" };
     url = url.replace(/:\/\/(m|www)\./g, "://");
-    const data = await sc.resolve.getV2(url, true).catch(() => undefined);
+    const data = await this.#sc.resolve.getV2(url, true).catch(() => undefined);
     if (!data || !["track", "playlist"].includes(data.kind)) {
       throw new DisTubeError("SOUNDCLOUD_PLUGIN_NOT_SUPPORTED", "Only public links are supported.");
     }
-    if (data.kind === "playlist") {
-      const playlist = new SoundCloudPlaylist(data);
-      if (!playlist.tracks?.length) throw new DisTubeError("SOUNDCLOUD_PLUGIN_EMPTY_PLAYLIST", "Playlist is empty.");
-      playlist.songs = (await resolveTracks(playlist.tracks)).map(s => new Song(new SoundCloudTrack(s), opt));
-      return new Playlist(playlist, opt);
-    } else {
-      return new Song(new SoundCloudTrack(data), opt);
-    }
+
+    return data.kind === "playlist"
+      ? new Playlist(new SoundCloudPlaylist(await this.#sc.playlists.fetch(data)), opt)
+      : new Song(new SoundCloudTrack(data), opt);
   }
 
   override async getRelatedSongs(url: string | number) {
-    const related = await sc.tracks.relatedV2(url, 10);
+    const related = await this.#sc.tracks.relatedV2(url, 10);
     return related.filter(t => t.title).map(t => new Song(new SoundCloudTrack(t)));
   }
 
   override async getStreamURL(url: string) {
-    const stream = await sc.util.streamLink(url);
+    const stream = await this.#sc.util.streamLink(url);
     if (!stream) {
       throw new DisTubeError(
         "SOUNDCLOUD_PLUGIN_RATE_LIMITED",
@@ -138,19 +136,19 @@ class SoundCloudTrack implements OtherSongInfo {
 
 class SoundCloudPlaylist implements PlaylistInfo {
   source: "soundcloud";
-  songs!: Song[];
+  songs: Song[];
   id: number;
   name: string;
   url: string;
   thumbnail?: string;
-  tracks: SoundcloudTrackV2[];
   constructor(info: SoundcloudPlaylistV2) {
     this.source = "soundcloud";
     this.id = info.id;
     this.name = info.title;
     this.url = info.permalink_url;
     this.thumbnail = info.artwork_url ?? undefined;
-    this.tracks = info.tracks;
+    if (!info.tracks?.length) throw new DisTubeError("SOUNDCLOUD_PLUGIN_EMPTY_PLAYLIST", "Playlist is empty.");
+    this.songs = info.tracks.map(s => new Song(new SoundCloudTrack(s)));
   }
 }
 
