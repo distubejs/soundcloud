b477fb6475e62ba80c67d2e230b7c0d8aec22116
Skick <skick1234@gmail.com>
2024-05-23 09:24:47 +0700

    chore: update API & dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import type { Options } from "tsup";
 export const tsup: Options = {
   clean: true,
   dts: true,
-  entry: ["index.ts"],
+  entry: ["src/index.ts"],
   format: "cjs",
   minify: false,
   keepNames: true,
